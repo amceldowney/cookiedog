38f310ead2bcda44d3104ca158cb487bbfd5669f
amceldowney <amceldowney@ucsb.edu>
2021-02-04 16:52:41 -0800

    test

--- R/amber_theme.R
+++ R/amber_theme.R
@@ -6,7 +6,7 @@
 #' @examples
 amber_theme <- function() {
   theme(
-    panel.background = element_rect(fill = "blue"),
+    panel.background = element_rect(fill = "blue"),dev
     panel.grid.major.x = element_line(colour = "skyblue", linetype = 3, size = 0.5),
     panel.grid.minor.x = element_blank(),
     panel.grid.major.y =  element_line(colour = "skyblue4", linetype = 3, size = 0.5),
